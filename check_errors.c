87e79146abf94ee4df2239018365a7c650b31bc7
naharagu <76852123+naharagu@users.noreply.github.com>
2022-11-01 11:00:10 +0100

    fix permisson

--- check_errors.c
+++ check_errors.c
@@ -6,7 +6,7 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 23:33:55 by naharagu          #+#    #+#             */
-/*   Updated: 2022/11/01 10:13:40 by naharagu         ###   ########.fr       */
+/*   Updated: 2022/11/01 10:59:59 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	check_args_and_init(int argc, char **argv, t_info *info)
 	info->fd_in = open(argv[1], O_RDONLY);
 	if (info->fd_in < 0)
 		put_perror_and_exit("open failed");
-	info->fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXO);
+	info->fd_out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (info->fd_out < 0)
 		put_perror_and_exit("open failed");
 	info->fd_pipe = malloc(sizeof(int) * 2);
